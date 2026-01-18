099b91b340d582362faf61c579dff8e1ce0c96d4
Gazda99 <jakub.gazdzik@outlook.com>
2026-01-18 15:36:48 +0100

    Update ICacheProvider.TryGet Obsolete message

--- YourGamesList.Common/Caching/ICacheProvider.cs
+++ YourGamesList.Common/Caching/ICacheProvider.cs
@@ -8,8 +8,9 @@ namespace YourGamesList.Common.Caching;
 
 public interface ICacheProvider
 {
-    [Obsolete]
+    [Obsolete("Use Get<T> instead.")]
     bool TryGet<T>(string key, [NotNullWhen(true)] out T? value, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default);
+
     Task<CombinedResult<T, CacheProviderError>> Get<T>(string key, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default);
     Task Set<T>(string key, T value, TimeSpan? expiration = null, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default);
     Task Remove(string key, CancellationToken cancellationToken = default);
