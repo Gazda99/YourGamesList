f768eacbdc0634461173de22d966814e12470aa7
Gazda99 <jakub.gazdzik@outlook.com>
2026-04-28 20:07:22 +0200

    Small updates

--- YourGamesList.Api/Telemetry/ActivitySources.cs
+++ YourGamesList.Api/Telemetry/ActivitySources.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using OpenTelemetry.Trace;
-using YourGamesList.Contracts.Dto;
 
 namespace YourGamesList.Api.Telemetry;
 
@@ -16,27 +14,6 @@ public static class ActivitySources
             var activity = HealthCheckActivitySource.StartActivity(name);
             return new HealthCheckActivity(activity);
         }
-    }
-}
-
-public readonly struct HealthCheckActivity : IDisposable
-{
-    private readonly Activity? _activity;
-
-    public HealthCheckActivity(Activity? activity)
-    {
-        _activity = activity;
-        _activity?.SetTag("is_health_check", "true");
-    }
-
-    public void SetHealthCheckStatus(HealthCheckStatusDto status)
-    {
-        _activity?.SetTag("health.status", status.ToString().ToUpper());
-    }
-
-    public void Dispose()
-    {
-        _activity?.Dispose();
     }
 }
 
